580b10558f513ef45805fb619a3148ff43807d44
theshuabhmy <theshubham.dev@gmail.com>
2026-01-02 05:18:51 +0530

    Refactor MainApplication to manually create package list, addressing null ReactNativeHost issue and ensuring proper package integration for React Native components.

--- android/app/src/main/java/com/spendwise/MainApplication.kt
+++ android/app/src/main/java/com/spendwise/MainApplication.kt
@@ -6,17 +6,43 @@ import com.facebook.react.ReactApplication
 import com.facebook.react.ReactHost
 import com.facebook.react.ReactNativeApplicationEntryPoint.loadReactNative
 import com.facebook.react.defaults.DefaultReactHost.getDefaultReactHost
+import java.util.ArrayList
 
 class MainApplication : Application(), ReactApplication {
 
   override val reactHost: ReactHost by lazy {
+    // Create packages list manually to avoid PackageList accessing null ReactNativeHost
+    val packageList = ArrayList<com.facebook.react.ReactPackage>()
+
+    // Get packages from PackageList but handle the null ReactNativeHost issue
+    val packageListInstance = PackageList(this)
+    // Access packages property which will trigger getPackages()
+    // But we need to work around the RNNotificationsPackage issue
+    try {
+      packageList.addAll(packageListInstance.packages)
+    } catch (e: NullPointerException) {
+      // If PackageList fails due to null ReactNativeHost, create packages manually
+      packageList.add(com.facebook.react.shell.MainReactPackage(null))
+      packageList.add(com.reactnativecommunity.asyncstorage.AsyncStoragePackage())
+      packageList.add(com.reactcommunity.rndatetimepicker.RNDateTimePickerPackage())
+      packageList.add(com.reactnativecommunity.picker.RNCPickerPackage())
+      packageList.add(com.reactnativevectoricons.ionicons.VectorIconsIoniconsPackage())
+      packageList.add(com.rnbiometrics.ReactNativeBiometricsPackage())
+      packageList.add(com.swmansion.gesturehandler.RNGestureHandlerPackage())
+      packageList.add(org.linusu.RNGetRandomValuesPackage())
+      packageList.add(com.oblador.keychain.KeychainPackage())
+      packageList.add(com.libsodium.LibsodiumPackage())
+      packageList.add(com.margelo.nitro.mmkv.NitroMmkvPackage())
+      packageList.add(com.margelo.nitro.NitroModulesPackage())
+      packageList.add(com.wix.reactnativenotifications.RNNotificationsPackage(this))
+      packageList.add(com.margelo.rnquicksqlite.SequelPackage())
+      packageList.add(com.th3rdwave.safeareacontext.SafeAreaContextPackage())
+      packageList.add(com.swmansion.rnscreens.RNScreensPackage())
+    }
+
     getDefaultReactHost(
-      context = applicationContext,
-      packageList =
-        PackageList(this).packages.apply {
-          // Packages that cannot be autolinked yet can be added manually here, for example:
-          // add(MyReactNativePackage())
-        },
+      context = this,
+      packageList = packageList,
     )
   }
 
